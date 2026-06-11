9dc537c80e6505e0ebebd451d7c13f9224cc1951
project3421699 <project3421699@gmail.com>
2026-06-11 07:20:42 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "pdfsquish-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./other.html",
+  "./pdf-splitter.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
